127320312005909abc91a121b118ac6cdcf8104b
Joanne Senoren <jnsenoren@gmail.com>
2020-01-15 18:29:20 +0400

    updated database urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
-	DB_URL:
-		process.env.DB_URL ||
+	DATABASE_URL:
+		process.env.DATABASE_URL ||
 		'postgres://word_nerd_user@localhost:5432/word_nerd',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 	CLIENT_ORIGIN: 'https://word-nerd.now.sh'
--- test/setup.js
+++ test/setup.js
@@ -4,8 +4,8 @@ process.env.JWT_SECRET = 'test-jwt-secret';
 
 require('dotenv').config();
 
-process.env.TEST_DB_URL =
-	process.env.TEST_DB_URL ||
+process.env.TEST_DATABASE_URL =
+	process.env.TEST_DATABASE_URL ||
 	'postgresql://word_nerd_user@localhost:5432/word_nerd_test';
 
 const { expect } = require('chai');
