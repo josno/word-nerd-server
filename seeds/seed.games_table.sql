45d287f2c398f79e63619b30ca101d69a3fd69c6
Joanne Senoren <jnsenoren@gmail.com>
2020-01-20 11:50:49 +0400

    updated

--- seeds/seed.games_table.sql
+++ seeds/seed.games_table.sql
@@ -1,21 +1,18 @@
 BEGIN;
 
-    TRUNCATE
+TRUNCATE
     games, users
     RESTART IDENTITY CASCADE;
 
-    INSERT INTO users
-        (user_name, full_name, password)
-    VALUES
-        ('nerd', 'Word Nerd', '$2a$12$CaTYM00gguRlZ9X73Fmum.fNBniS04q2eQKEwN1YRH2rq35HUhs/e'),
-        ('thinkful', 'Thinkful User', '$2a$16$ZkUvK7yezCNNqbSF5Kh06.wGbHJJWRTNgrRiRvPkB80IFQ6wrMFiG');
+INSERT INTO users (user_name, full_name, password)
+VALUES 
+('nerd', 'Word Nerd', '$2a$12$CaTYM00gguRlZ9X73Fmum.fNBniS04q2eQKEwN1YRH2rq35HUhs/e'),
+('thinkful', 'Thinkful User', '$2a$16$ZkUvK7yezCNNqbSF5Kh06.wGbHJJWRTNgrRiRvPkB80IFQ6wrMFiG');
 
-    INSERT INTO games
-        (title, word_list, user_id)
-    VALUES
-        ('Colors', ARRAY
-    ['red', 'blue', 'pink', 'orange', 'green', 'purple', 'mint', 'black', 'white'], 1),
-    ('Animals', ARRAY['bird', 'turtle', 'horse', 'fish', 'alligator', 'cheetah', 'lion', 'tiger', 'bear'], 1),
-    ('Places', ARRAY['library', 'school', 'park', 'restaurant', 'museum', 'cafe', 'bakery', 'store', 'market', 'hospital'], 2);
+INSERT INTO games (title, word_list, user_id)
+VALUES 
+('Colors', ARRAY['red', 'blue', 'pink', 'orange', 'green', 'purple', 'mint', 'black', 'white'], 1),
+('Animals', ARRAY['bird', 'turtle', 'horse', 'fish', 'alligator', 'cheetah', 'lion', 'tiger', 'bear'], 1),
+('Places', ARRAY['library', 'school', 'park', 'restaurant', 'museum', 'cafe', 'bakery', 'store', 'market', 'hospital'], 2);
 
 COMMIT;
--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
 		process.env.DATABASE_URL ||
 		'postgres://word_nerd_user@localhost:5432/word_nerd',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-	CLIENT_ORIGIN: 'https://word-nerd.now.sh'
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://word-nerd.now.sh'
 };
