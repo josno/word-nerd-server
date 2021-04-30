9c4ca81b06d15c91f5eae70379c3cd1b1375b007
josno <jnsenoren@gmail.com>
2021-05-01 01:07:21 +0400

    Updated

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
-const gamesRouter = require('./games/games-router');
-const authRouter = require('./auth/auth-router');
-const usersRouter = require('./users/users-router');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
+const gamesRouter = require("./games/games-router");
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.options('*', cors());
+app.options("*", cors());
 app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(helmet());
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!');
+app.get("/", (req, res) => {
+	res.send("Hello, world!");
 });
 
-app.use('/api/v1/games', gamesRouter);
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', usersRouter);
+app.use("/api/v1/games", gamesRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
 	let response;
-	if (NODE_ENV === 'production') {
-		response = { error: { message: 'server error' } };
+	if (NODE_ENV === "production") {
+		response = { error: { message: "server error" } };
 	} else {
 		console.error(error);
 		response = { message: error.message, error };
--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	PORT: process.env.PORT || 8000,
-	NODE_ENV: process.env.NODE_ENV || 'development',
-	DATABASE_URL:
-		process.env.DATABASE_URL ||
-		'postgres://word_nerd_user@localhost:5432/word_nerd',
-	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
-	// CLIENT_ORIGIN: 'http://localhost:3000'
-};
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgres://word_nerd_user@localhost:5432/word_nerd',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+  // CLIENT_ORIGIN: "http://localhost:3000",
+}
